a83b6132c9e109886fae6d1b9b118891a389b4b3
jared-limjoco <limjocojared9@gmail.com>
2022-12-06 08:41:55 +0800

    Update ClientFunctions.py

--- ClientFunctions.py
+++ ClientFunctions.py
@@ -3,18 +3,50 @@ import socket
 BUFFER_SIZE = 1024
 
 def helpMenu():
-    print("Connect to the server application: /join <server_ip_add> <port>")
-    print("Disconnect to the server application: /leave")
-    print("Register a unique handle or alias: /register <handle>")
-    print("Send message to all: /all <message>")
-    print("Send direct message to a single handle: /msg <handle> <message>")
-    print("Request command help to output all Input Syntax commands for references: /?")
-    print("\n\nAdditonal Features:")
-    print("Creating a group chat: /createGC <group chat name>")
-    print("Adding users to group chat: /addGC <group chat name> <user handle to add>")
-    print("Leaving a group chat: /leaveGC <group chat name>")
-    print("Messaging a group chat: /msgGC <group chat name> <message>")
-    print("\nEmoji commands: \n ':happy:': '😊',\n':sad:': '😢',\n':laugh:':'😂',\n':angry:': '😡'")
+    print("""
++------------------------------+----------------------------------------+
+|                               HELP MENU                               |
++------------------------------+----------------------------------------+
+| /?                           | Request command help to output all     |
+|                              | Input Syntax commands for references.  |
++------------------------------+----------------------------------------+
+| /join <server_ip_add> <port> | Connect to the server application.     |
++------------------------------+----------------------------------------+
+| /leave                       | Disconnect to the server application.  |
++------------------------------+----------------------------------------+
+| /register <handle>           | Register a unique handle or alias.     |
++------------------------------+----------------------------------------+
+| /all <message>               | Send message to all.                   |
++------------------------------+----------------------------------------+
+| /msg <handle> <message>      | Send direct message to a single        |
+|                              | handle.                                |
++------------------------------+----------------------------------------+
+
++-----------+----+
+| LIST OF EMOJIS |
++-----------+----+
+| :happy:   | 😊 |         
++-----------+----+
+| :sad:     | 😢 |
++-----------+----+
+| :laugh:   | 😂 |         
++-----------+----+
+| :angry:   | 😡 |         
++-----------+----+
+
++------------------------------+----------------------------------------+
+|                          GROUP CHAT COMMANDS                          |
++------------------------------+----------------------------------------+
+| /createGC <group chat name>  | Creates a new group chat.              |
++------------------------------+----------------------------------------+
+| /addGC <group chat name>     | Adds a user to a group chat.           |
+| <user handle to add>         |                                        |
++------------------------------+----------------------------------------+
+| /msgGC <group chat name>     | Send a message to a group chat         |
+| <message>                    |                                        |
++------------------------------+----------------------------------------+
+| /leaveGC <group chat name>   | Leave a group chat.                    |
++------------------------------+----------------------------------------+""")
 
 def receiveThread(connection):
     while(True):
